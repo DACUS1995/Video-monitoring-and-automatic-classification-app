1d37917c9d4721162e1f1818413403a2cc1cff87
DACUS1995 <studormarian@yahoo.co.uk>
2018-06-28 23:10:43 +0300

    cleaning stuff

--- remoteApp/RemoteElectron/index.js
+++ remoteApp/RemoteElectron/index.js
@@ -20,55 +20,55 @@ ipcRenderer.on("updateViewConfig", (event, data) => {
 	ElectronRendererSocketHandler.handleIncomingMessage(data);
 });
 
-window.addEventListener('load', function (evt) {
+window.addEventListener('load', async function (evt) {
 	let elVideo = document.getElementById('remoteVideo');
 	let stream;
 	let startTime;
 
-	navigator.mediaDevices.enumerateDevices()
-	.then((arrDevices) => {
-		let objVideoConstraints = null;
+	const arrDevices = await navigator.mediaDevices.enumerateDevices();
+	let objVideoConstraints = null;
 
-		for(let device of arrDevices)
+	for(let device of arrDevices)
+	{
+		// Windows only and only for external classification process
+		if(device.label == "Multisplitter Video Source")
 		{
-			
-
-			// Windows only and only for external classification process
-			if(device.label == "Multisplitter Video Source")
-			{
-				objVideoConstraints = {
-					deviceId: {exact: device.deviceId}
-				};
-				break;
-			}
+			objVideoConstraints = {
+				deviceId: {exact: device.deviceId}
+			};
+			break;
 		}
+	}
 
-		if(objVideoConstraints == null)
-		{
-			// throw new Error("Multisplitter Video Source is not available");
-		}
+	if(objVideoConstraints == null)
+	{
+		// throw new Error("Multisplitter Video Source is not available");
+	}
 		
-		navigator.mediaDevices.getUserMedia({audio: false, video: true /*video: objVideoConstraints*/}) 
-		.then(function (localStream) {
-			elVideo.src = window.URL.createObjectURL(localStream);
-			console.log(`Video Url: ${elVideo.src}`);
-	
-			let objRemoteWebRTC = new remoteWebRTC(localStream, SimplePeer, ipcRenderer);
-	
-			setTimeout(() => {
-				objRemoteWebRTC.createConnection();
-			}, 3500);
-		})
-		.catch(function (err) {
-			console.log('The following error occurred: ' + err.name)
-		})
-	});
+	navigator.mediaDevices.getUserMedia({audio: false, video: true /*video: objVideoConstraints*/}) 
+	.then(function (localStream) 
+	{
+		elVideo.src = window.URL.createObjectURL(localStream);
+		console.log(`Video Url: ${elVideo.src}`);
 
-	// Pass the elDivInstructionContainer to the correct data classfier handlers
-	const elDivInstructionContainer = document.getElementById("instructions-container");
-	const objGraphicRouter = new GraphicRouting(elDivInstructionContainer);
+		// Wait for some time to make sure all the things are ready
+		setTimeout(() => 
+		{
+			const objRemoteWebRTC = new remoteWebRTC(localStream, SimplePeer, ipcRenderer);
+			objRemoteWebRTC.createConnection();
+			
+			// Pass the elDivInstructionContainer to the correct data classfier handlers
+			const elDivInstructionContainer = document.getElementById("instructions-container");
+			const objGraphicRouter = new GraphicRouting(elDivInstructionContainer);
+		
+			const objTFjsClassifier = new TFjsClassifier(elVideo, objGraphicRouter);
+			objTFjsClassifier.runClassifier();
+		}, 3500);
+	})
+	.catch(function (err) 
+	{
+		console.log('The following error occurred: ' + err.name)
+	})
 
-	const objTFjsClassifier = new TFjsClassifier(elVideo, objGraphicRouter);
-	objTFjsClassifier.runClassifier();
 
 })
